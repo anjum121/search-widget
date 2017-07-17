110ed8addfb9ec4315516c28a0ffcbd5c436d21f
Anjum121 <anjum.nawab@mercator.com>
2017-07-17 13:20:05 +0530

    search Widget component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule,
